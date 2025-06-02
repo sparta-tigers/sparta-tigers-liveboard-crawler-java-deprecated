db5f9eea2265b366db2957aeb015a9a0f74e504f
김정민 <raon37910@gmail.com>
2025-06-02 13:13:06 +0900

    Initial Commit

---
+++ src/main/java/com/sparta/SprataTigersLiveboardCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprataTigersLiveboardCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprataTigersLiveboardCrawlerApplication.class, args);
+    }
+
+}
